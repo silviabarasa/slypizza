8e318a0e34e14dad0b2b6219e6c850634af84e7e
silviabarasa16 <silviabarasa16@gmail.com>
2019-10-05 19:53:26 +0300

    add variables for pizza selection and an alert function

--- js/scripts.js
+++ js/scripts.js
@@ -139,5 +139,61 @@ function Order(name, pizza, topping, crust, boxes, delivery) {
           var pizzaPrices = [400, 600, 1000, 600, 800, 1200, 650, 850, 1250, 500, 700, 1100, 550, 750, 1150, 700, 900, 1300];
           var toppingPrices = [0, 100, 200, 200, 50, 250, 300];
           var crustPrices = [200, 0];
-        });
+          
+    var priceOfSelectedPizza = pizzaPrices[selectedPizza-1];
+    var priceOfToppingSelected = toppingPrices[toppingSelected];
+    var priceOfCrustSelected = crustPrices[crustSelected-1];
+
+    var nameOfPizza = pizzas[selectedPizza-1];
+    var nameOfTopping = toppings[toppingSelected-1];
+    var nameOfCrust = crusts[crustSelected-1];
+    var nameOfDeliveryOption = deliveryOptions[selectedDeliveryOption-1];
+
+
+    var newOrder = new Order(inputtedName, nameOfPizza, nameOfTopping, nameOfCrust, noOfBoxes, nameOfDeliveryOption);
+    var newPrices = new Price(priceOfSelectedPizza, priceOfToppingSelected, priceOfCrustSelected);
+    var newSubtotal = newPrices.subtotal();
+    var newBill = new Bill(newSubtotal, noOfBoxes);
+    var newTotal = newBill.total();
+
+
+
+    if (selectedDeliveryOption == "2"){
+      $('#address').show();
+
+    }
+    else{
+      document.getElementById('order-summary').value = newOrder.selectedPizza + ' * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedPizza * newOrder.numberOfBoxes + '\n' + newOrder.selectedTopping + ' topping * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedTopping * newOrder.numberOfBoxes + '\n' + newOrder.selectedCrust + ' crust * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedCrust * newOrder.numberOfBoxes + '\n' + 'Total: ' + newTotal;
+    }
+
+    $('form#delivery-address').submit(function(event){
+      event.preventDefault();
+
+
+      document.getElementById('order-summary').value = newOrder.selectedPizza + ' * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedPizza * newOrder.numberOfBoxes + '\n' + newOrder.selectedTopping + ' topping * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedTopping * newOrder.numberOfBoxes + '\n' + newOrder.selectedCrust + ' crust * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedCrust * newOrder.numberOfBoxes + '\n' + 'Total: ' + newTotal;
     });
+
+    $('form#checkout-form').submit(function(event){
+      event.preventDefault();
+
+      var inputtedCity = $('input#city').val();
+      var inputtedEstate = $('input#estate').val();
+      var inputtedNumber = $('#mobile').val();
+
+      var newAddress = new Address(inputtedCity, inputtedEstate, inputtedNumber);
+
+      if(selectedDeliveryOption == "2"){
+
+        alert(newOrder.customerName + '. Your order will be delivered to ' + newAddress.estateName + ', ' + newAddress.cityName + ' in the next 30 minutes. We will call this number (' + newAddress.customerMobile + ') when we get there. Thank you for being a loyal l-unico customer');
+      }
+      else{
+        alert(newOrder.customerName +', your order will be ready for collection in 25 minutes.  Thank you for being a loyal customer');
+      }
+
+      document.getElementById("checkout-form").reset();
+      document.getElementById("new-order").reset();
+      document.getElementById("delivery-address").reset();
+      $('#address').hide();
+    });
+  });
+});
