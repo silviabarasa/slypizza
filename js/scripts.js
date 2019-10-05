db05764fd29ca5ac5246f9ec19d95740911c9396
silviabarasa16 <silviabarasa16@gmail.com>
2019-10-05 19:37:44 +0300

    add a prototype for the price and function

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,12 @@ function Order(name, pizza, topping, crust, boxes, delivery) {
     this.estateName = estate;
     this.customerMobile = mobileNumber;
   }
-    
+  function Price(pizzaPrice, toppingPrice, crustPrice){
+    this.priceForSelectedPizza = pizzaPrice;
+    this.priceForSelectedTopping = toppingPrice;
+    this.priceForSelectedCrust = crustPrice;
+  }
+  
+  Price.prototype.subtotal = function(){
+    return this.priceForSelectedPizza + this.priceForSelectedTopping + this.priceForSelectedCrust;
+  }
