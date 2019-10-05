c7a49bd1fb2543582c54ad897d94f012b042225d
silviabarasa16 <silviabarasa16@gmail.com>
2019-10-05 19:20:06 +0300

    Add a function for the form

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function Order(name, pizza, topping, crust, boxes, delivery) {
+    this.customerName = name;
+    this.selectedPizza = pizza;
+    this.selectedTopping = topping;
+    this.selectedCrust = crust;
+    this.numberOfBoxes = boxes;
+    this.deliveryOption = delivery;
+  }
+  
