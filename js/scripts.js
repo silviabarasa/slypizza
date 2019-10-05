92bb6cf23bd62696216fd9663bb86eff1fcd5f40
silviabarasa16 <silviabarasa16@gmail.com>
2019-10-05 19:47:47 +0300

    add function for toppings using if else

--- js/scripts.js
+++ js/scripts.js
@@ -71,4 +71,50 @@ function Order(name, pizza, topping, crust, boxes, delivery) {
   }
   
   navSlide();
+  function switchTopping() {
+    if (document.getElementById('toppings')) {
   
+        if (document.getElementById('toppings').style.display == 'none') {
+            document.getElementById('toppings').style.display = 'block';
+            document.getElementById('classic').style.display = 'none';
+            document.getElementById('deluxe').style.display = 'none';
+        }
+        else {
+            document.getElementById('toppings').style.display = 'block';
+            document.getElementById('classic').style.display = 'none';
+            document.getElementById('deluxe').style.display = 'none';
+        }
+      }
+    }
+  
+  function switchClassic() {
+  if (document.getElementById('classic')) {
+  
+      if (document.getElementById('classic').style.display == 'none') {
+          document.getElementById('toppings').style.display = 'none';
+          document.getElementById('classic').style.display = 'block';
+          document.getElementById('deluxe').style.display = 'none';
+      }
+      else {
+          document.getElementById('toppings').style.display = 'none';
+          document.getElementById('classic').style.display = 'block';
+          document.getElementById('deluxe').style.display = 'none';
+      }
+    }
+  }
+  
+  function switchDeluxe() {
+    if (document.getElementById('deluxe')) {
+  
+        if (document.getElementById('deluxe').style.display == 'none') {
+            document.getElementById('toppings').style.display = 'none';
+            document.getElementById('classic').style.display = 'none';
+            document.getElementById('deluxe').style.display = 'block';
+        }
+        else {
+            document.getElementById('toppings').style.display = 'none';
+            document.getElementById('classic').style.display = 'none';
+            document.getElementById('deluxe').style.display = 'block';
+        }
+      }
+    }  
